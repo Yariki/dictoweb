cb3d5586b21c1bbd32ccf2cde4b4a6d3b180501e
yariki <iyariki.ya@gmail.com>
2019-05-18 11:42:08 +0300

    added docker-compose script for running MS sql server in container

--- DictoData/Context/DictoContext.cs
+++ DictoData/Context/DictoContext.cs
@@ -23,7 +23,9 @@ namespace DictoData.Context
         
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=dictodb;Persist Security Info=True;User ID=sa;Password=123");
+            //optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=dictodb;Persist Security Info=True;User ID=sa;Password=123");
+            optionsBuilder.UseSqlServer("server=localhost,1434; user id = sa; password=8jkGh47hnDw89Haq8LN2; database=dictodb;");
+            
         }
     }
 }
